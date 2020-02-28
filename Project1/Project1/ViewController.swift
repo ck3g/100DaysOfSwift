0ad662e6c16a68e5245f3027efca84a065d2b9f1
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-02-28 08:46:19 +0100

    Display a list of image names in the table view

--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -25,6 +25,15 @@ class ViewController: UITableViewController {
     }
   }
 
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return pictures.count
+  }
 
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+    cell.textLabel?.text = pictures[indexPath.row]
+
+    return cell
+  }
 }
 
