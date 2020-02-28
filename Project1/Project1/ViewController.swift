d78d911a90383af9874dabc60f5b2268b56ac6ec
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-02-28 07:48:32 +0100

    Create a blank Project1

---
+++ Project1/Project1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Vitali Tatarintev on 28.02.20.
+//  Copyright © 2020 Vitali Tatarintev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ Project1/README.md
@@ -0,0 +1,6 @@
+# The first project
+
+The first project of 100 Days of Swift challenge.
+
+More info about the project [here](https://www.hackingwithswift.com/read/1/overview).
+
