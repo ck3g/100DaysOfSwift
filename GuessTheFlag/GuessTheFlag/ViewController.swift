baaca1b262e01f12ec35fca30c7dd76f36f09389
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-03-01 09:42:03 +0100

    Build the app interface

--- GuessTheFlag/GuessTheFlag/ViewController.swift
+++ GuessTheFlag/GuessTheFlag/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+  @IBOutlet var button1: UIButton!
+  @IBOutlet var button2: UIButton!
+  @IBOutlet var button3: UIButton!
 
   override func viewDidLoad() {
     super.viewDidLoad()
