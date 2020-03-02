99f2a8b259a994d844636232c04891b5c4a05f85
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-03-02 09:33:57 +0100

    Make buttons tappable

--- GuessTheFlag/GuessTheFlag/ViewController.swift
+++ GuessTheFlag/GuessTheFlag/ViewController.swift
@@ -33,7 +33,7 @@ class ViewController: UIViewController {
     askQuestion()
   }
 
-  func askQuestion() {
+  func askQuestion(action: UIAlertAction! = nil) {
     countries.shuffle()
     correctAnswer = Int.random(in: 0...2)
 
@@ -44,5 +44,19 @@ class ViewController: UIViewController {
     title = countries[correctAnswer].uppercased()
   }
 
-}
+  @IBAction func buttonTapped(_ sender: UIButton) {
+    var title: String
 
+    if sender.tag == correctAnswer {
+      title = "Correct"
+      score += 1
+    } else {
+      title = "Wrong"
+      score -= 1
+    }
+
+    let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+    ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+    present(ac, animated: true)
+  }
+}
