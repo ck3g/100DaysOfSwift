7cccc5735e614852f555c805535db05011817056
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-03-03 07:46:50 +0100

    Make a copy of the first project

---
+++ 03-TechniqueProject/Project1/DetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Vitali Tatarintev on 29.02.20.
+//  Copyright © 2020 Vitali Tatarintev. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+  @IBOutlet var imageView: UIImageView!
+
+  var selectedImage: String?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    title = selectedImage
+    navigationItem.largeTitleDisplayMode = .never
+    
+    if let imageToLoad = selectedImage {
+      imageView.image = UIImage(named: imageToLoad)
+    }
+  }
+
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    navigationController?.hidesBarsOnTap = true
+  }
+
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    navigationController?.hidesBarsOnTap = false
+  }
+
+}
---
+++ 03-TechniqueProject/README.md
@@ -0,0 +1,6 @@
+# Technique Project
+
+A project of 100 Days of Swift challenge.
+
+More info about the project [here](https://www.hackingwithswift.com/100/22).
+
