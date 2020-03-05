6967a85237c960396b98ebe7a8528162ece7d767
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-03-05 03:17:35 +0100

    Add DetailView

---
+++ FlagsFlags/FlagsFlags/DetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailViewController.swift
+//  FlagsFlags
+//
+//  Created by Vitali Tatarintev on 05.03.20.
+//  Copyright © 2020 Vitali Tatarintev. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+  @IBOutlet var flagImage: UIImageView!
+  @IBOutlet var countryLabel: UILabel!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+}
