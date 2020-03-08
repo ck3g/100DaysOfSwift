a8c76a21a357acbad8099787eb4d5655f4f9c39d
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-03-08 09:14:06 +0100

    Add a bottom toolbar with a refresh button

--- 04-EasyBrowser/EasyBrowser/ViewController.swift
+++ 04-EasyBrowser/EasyBrowser/ViewController.swift
@@ -23,6 +23,12 @@ class ViewController: UIViewController, WKNavigationDelegate {
 
     navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
 
+    let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+    let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+
+    toolbarItems = [spacer, refresh]
+    navigationController?.isToolbarHidden = false
+
     let url = URL(string: "https://hackingwithswift.com")!
     webView.load(URLRequest(url: url))
     webView.allowsBackForwardNavigationGestures = true
