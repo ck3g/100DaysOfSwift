9378e75187a1e6a3e01c04f1425374f71bc6ad88
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-03-08 09:22:52 +0100

    Add a progress bar to indicate page loading progress

--- 04-EasyBrowser/EasyBrowser/ViewController.swift
+++ 04-EasyBrowser/EasyBrowser/ViewController.swift
@@ -11,6 +11,7 @@ import WebKit
 
 class ViewController: UIViewController, WKNavigationDelegate {
   var webView: WKWebView!
+  var progressView: UIProgressView!
 
   override func loadView() {
     webView = WKWebView()
@@ -26,8 +27,14 @@ class ViewController: UIViewController, WKNavigationDelegate {
     let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
     let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
 
-    toolbarItems = [spacer, refresh]
+    progressView = UIProgressView(progressViewStyle: .default)
+    progressView.sizeToFit()
+    let progressButton = UIBarButtonItem(customView: progressView)
+
+    toolbarItems = [progressButton, spacer, refresh]
     navigationController?.isToolbarHidden = false
+
+    webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
 
     let url = URL(string: "https://hackingwithswift.com")!
     webView.load(URLRequest(url: url))
@@ -52,6 +59,12 @@ class ViewController: UIViewController, WKNavigationDelegate {
 
   func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
     title = webView.title
+  }
+
+  override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+    if keyPath == "estimatedProgress" {
+      progressView.progress = Float(webView.estimatedProgress)
+    }
   }
 }
 
