d2a33d596af264f328da2f0484f4c09f77b6cbc4
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-03-08 08:49:15 +0100

    Load a web page using WebKit

--- 04-EasyBrowser/EasyBrowser/ViewController.swift
+++ 04-EasyBrowser/EasyBrowser/ViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
+  var webView: WKWebView!
+
+  override func loadView() {
+    webView = WKWebView()
+    webView.navigationDelegate = self
+    view = webView
+  }
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+
+    let url = URL(string: "https://hackingwithswift.com")!
+    webView.load(URLRequest(url: url))
+    webView.allowsBackForwardNavigationGestures = true
   }
 
 
