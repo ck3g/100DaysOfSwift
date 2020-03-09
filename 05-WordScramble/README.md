cff24236ef58fa70776dbac1b48764fbe90c93d0
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-03-09 09:57:06 +0100

    Create a blank Word Scramble project

---
+++ 05-WordScramble/README.md
@@ -0,0 +1,4 @@
+# Word Scramble
+
+The 5th project from 100 Days of Swift. More info [here](https://www.hackingwithswift.com/read/5/1/setting-up).
+
---
+++ 05-WordScramble/WordScramble/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordScramble
+//
+//  Created by Vitali Tatarintev on 09.03.20.
+//  Copyright © 2020 Vitali Tatarintev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
