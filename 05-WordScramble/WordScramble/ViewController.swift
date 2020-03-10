080151eaa9369c4ac72b09dca071496b290e36ed
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-03-10 06:25:11 +0100

    Show a prompt for answer

--- 05-WordScramble/WordScramble/ViewController.swift
+++ 05-WordScramble/WordScramble/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UITableViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
 
+    navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+
     if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
       if let startWords  = try? String(contentsOf: startWordsURL) {
         allWords = startWords.components(separatedBy: "\n")
@@ -43,6 +45,23 @@ class ViewController: UITableViewController {
     cell.textLabel?.text = usedWords[indexPath.row]
 
     return cell
+  }
+
+  @objc func promptForAnswer() {
+    let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+    ac.addTextField()
+
+    let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak ac] _ in
+      guard let answer = ac?.textFields?[0].text else { return }
+      self?.submit(answer)
+    }
+
+    ac.addAction(submitAction)
+    present(ac, animated: true)
+  }
+
+  func submit(_ answer: String) {
+
   }
 
 }
