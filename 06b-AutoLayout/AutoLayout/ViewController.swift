5382d4ff6f7cf020271613fea5cc9938a4e305ef
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-03-13 10:38:53 +0100

    Render labels using auto layour anchors

--- 06b-AutoLayout/AutoLayout/ViewController.swift
+++ 06b-AutoLayout/AutoLayout/ViewController.swift
@@ -49,21 +49,40 @@ class ViewController: UIViewController {
     view.addSubview(label4)
     view.addSubview(label5)
 
-    let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
 
-    for label in viewsDictionary.keys {
-      view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+    // Format using Visual Format
+//    let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
+//
+//    for label in viewsDictionary.keys {
+//      view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+//    }
+//
+//    // Manual sizes
+//    // view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(==88)]-[label2(==88)]-[label3(==88)]-[label4(==88)]-[label5(==88)]-(>=10)-|", options: [], metrics: nil, views: viewsDictionary))
+//
+//    let metrics = ["labelHeight": 88]
+//    // The sizes extracted into metrics
+//    // view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight)]-[label2(labelHeight)]-[label3(labelHeight)]-[label4(labelHeight)]-[label5(labelHeight)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary))
+//
+//    // Use the same sizes as the first label
+//    view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary))
+
+
+    // Format using Layout Anchors
+    var previous: UILabel?
+
+    for label in [label1, label2, label3, label4, label5] {
+      label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+      label.heightAnchor.constraint(equalToConstant: 88).isActive = true
+
+      if let previous = previous {
+        label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+      } else {
+        label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+      }
+
+      previous = label
     }
-
-    // Manual sizes
-    // view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(==88)]-[label2(==88)]-[label3(==88)]-[label4(==88)]-[label5(==88)]-(>=10)-|", options: [], metrics: nil, views: viewsDictionary))
-
-    let metrics = ["labelHeight": 88]
-    // The sizes extracted into metrics
-    // view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight)]-[label2(labelHeight)]-[label3(labelHeight)]-[label4(labelHeight)]-[label5(labelHeight)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary))
-
-    // Use the same sizes as the first label
-    view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary))
   }
 
 
