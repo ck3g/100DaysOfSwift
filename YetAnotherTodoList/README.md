1ee5085d2f9ea2a719a73586d3e55799257ea91d
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-03-14 13:51:11 +0100

    Create a blank Yet Another TODO List project

---
+++ YetAnotherTodoList/README.md
@@ -0,0 +1,5 @@
+# Yet Another TODO List
+
+A challenge project from 100 Days of Swift.
+
+More info [here](https://www.hackingwithswift.com/guide/3/3/challenge).
---
+++ YetAnotherTodoList/YetAnotherTodoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YetAnotherTodoList
+//
+//  Created by Vitali Tatarintev on 14.03.20.
+//  Copyright © 2020 Vitali Tatarintev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
