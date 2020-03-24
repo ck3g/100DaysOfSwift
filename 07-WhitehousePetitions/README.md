875f201294d5ed1132862b350a42b22445614b4a
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-03-24 20:39:22 +0100

    Create a blank Whitehouse Petitions project

---
+++ 07-WhitehousePetitions/README.md
@@ -0,0 +1,4 @@
+# Whitehouse Petitions
+
+The 7th project from 100 Days of Swift. More info [here](https://www.hackingwithswift.com/read/7/1/setting-up).
+
---
+++ 07-WhitehousePetitions/WhitehousePetitions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhitehousePetitions
+//
+//  Created by Vitali Tatarintev on 24.03.20.
+//  Copyright © 2020 Vitali Tatarintev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
