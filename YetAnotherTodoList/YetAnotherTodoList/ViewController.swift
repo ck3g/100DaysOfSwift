9bef876d7ec28e3309a622aa8b5561380192f07b
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-03-24 07:27:31 +0100

    Display a static list of items in the table view

--- YetAnotherTodoList/YetAnotherTodoList/ViewController.swift
+++ YetAnotherTodoList/YetAnotherTodoList/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UITableViewController {
+  var items = ["Do this", "Do that", "Also, do this", "And also, do that"]
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
 
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    items.count
+  }
 
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItem", for: indexPath)
+    cell.textLabel?.text = items[indexPath.row]
+
+    return cell
+  }
 }
 
