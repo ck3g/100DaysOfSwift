31a0c02d9c8b4c59a054495ddd96647dfcfd633f
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-03-24 21:15:22 +0100

    Display petitions details in a detail view

--- 07-WhitehousePetitions/WhitehousePetitions/ViewController.swift
+++ 07-WhitehousePetitions/WhitehousePetitions/ViewController.swift
@@ -44,5 +44,11 @@ class ViewController: UITableViewController {
 
     return cell
   }
+
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    let vc = DetailViewController()
+    vc.detailItem = petitions[indexPath.row]
+    navigationController?.pushViewController(vc, animated: true)
+  }
 }
 
