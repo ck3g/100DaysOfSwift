5b464e62ccf4ba25ac8b5da557cb2c687d6fc28d
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-03-24 21:27:07 +0100

    Display a second tab with popular petitions

--- 07-WhitehousePetitions/WhitehousePetitions/ViewController.swift
+++ 07-WhitehousePetitions/WhitehousePetitions/ViewController.swift
@@ -14,7 +14,13 @@ class ViewController: UITableViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+    let urlString: String
+
+    if navigationController?.tabBarItem.tag == 0 {
+      urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+    } else {
+      urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+    }
 
     if let url = URL(string: urlString) {
       if let data = try? Data(contentsOf: url) {
