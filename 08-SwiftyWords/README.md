e3f2e8ccf594431f8ac7d8964411f737e00da654
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-03-25 07:51:13 +0100

    Create a blank SwiftyWords project

---
+++ 08-SwiftyWords/README.md
@@ -0,0 +1,4 @@
+# Swifty Words
+
+The 8th project from 100 Days of Swift. More info [here](https://www.hackingwithswift.com/read/8/1/setting-up).
+
---
+++ 08-SwiftyWords/SwiftyWords/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftyWords
+//
+//  Created by Vitali Tatarintev on 25.03.20.
+//  Copyright © 2020 Vitali Tatarintev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
