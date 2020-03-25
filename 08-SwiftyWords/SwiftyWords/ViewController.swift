611c54ef22bb6edd31687ffcaec016fe2a5c526f
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-03-25 09:04:03 +0100

    Render Submit and Clear buttons

--- 08-SwiftyWords/SwiftyWords/ViewController.swift
+++ 08-SwiftyWords/SwiftyWords/ViewController.swift
@@ -48,6 +48,16 @@ class ViewController: UIViewController {
     currentAnswer.isUserInteractionEnabled = false
     view.addSubview(currentAnswer)
 
+    let submit = UIButton(type: .system)
+    submit.translatesAutoresizingMaskIntoConstraints = false
+    submit.setTitle("SUBMIT", for: .normal)
+    view.addSubview(submit)
+
+    let clear = UIButton(type: .system)
+    clear.translatesAutoresizingMaskIntoConstraints = false
+    clear.setTitle("CLEAR", for: .normal)
+    view.addSubview(clear)
+
     NSLayoutConstraint.activate([
       scoreLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
       scoreLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
@@ -63,7 +73,15 @@ class ViewController: UIViewController {
 
       currentAnswer.centerXAnchor.constraint(equalTo: view.centerXAnchor),
       currentAnswer.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
-      currentAnswer.topAnchor.constraint(equalTo: cluesLabel.bottomAnchor, constant: 20)
+      currentAnswer.topAnchor.constraint(equalTo: cluesLabel.bottomAnchor, constant: 20),
+
+      submit.topAnchor.constraint(equalTo: currentAnswer.bottomAnchor),
+      submit.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -100),
+      submit.heightAnchor.constraint(equalToConstant: 44),
+
+      clear.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 100),
+      clear.centerYAnchor.constraint(equalTo: submit.centerYAnchor),
+      clear.heightAnchor.constraint(equalToConstant: 44)
     ])
 
     cluesLabel.backgroundColor = .red
