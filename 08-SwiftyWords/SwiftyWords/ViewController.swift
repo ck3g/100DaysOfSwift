a9cf9db3f6b12024745422561962d6ff113c388f
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-03-25 08:15:25 +0100

    Render clue and answers labels

--- 08-SwiftyWords/SwiftyWords/ViewController.swift
+++ 08-SwiftyWords/SwiftyWords/ViewController.swift
@@ -25,10 +25,36 @@ class ViewController: UIViewController {
     scoreLabel.text = "Score: 0"
     view.addSubview(scoreLabel)
 
+    cluesLabel = UILabel()
+    cluesLabel.translatesAutoresizingMaskIntoConstraints = false
+    cluesLabel.font = UIFont.systemFont(ofSize: 24)
+    cluesLabel.text = "CLUES"
+    cluesLabel.numberOfLines = 0
+    view.addSubview(cluesLabel)
+
+    answersLabel = UILabel()
+    answersLabel.translatesAutoresizingMaskIntoConstraints = false
+    answersLabel.font = UIFont.systemFont(ofSize: 24)
+    answersLabel.text = "ANSWERS"
+    answersLabel.numberOfLines = 0
+    view.addSubview(answersLabel)
+
     NSLayoutConstraint.activate([
       scoreLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
-      scoreLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor)
+      scoreLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+
+      cluesLabel.topAnchor.constraint(equalTo: scoreLabel.bottomAnchor),
+      cluesLabel.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor, constant: 100),
+      cluesLabel.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor, multiplier: 0.6, constant: -100),
+
+      answersLabel.topAnchor.constraint(equalTo: scoreLabel.bottomAnchor),
+      answersLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor, constant: -100),
+      answersLabel.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor, multiplier: 0.4, constant: -100),
+      answersLabel.heightAnchor.constraint(equalTo: cluesLabel.heightAnchor)
     ])
+
+    cluesLabel.backgroundColor = .red
+    answersLabel.backgroundColor = .blue
   }
 
   override func viewDidLoad() {
