1a1f24afa2d5b8de8b4696c4771c3feb02577981
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-03-25 08:05:53 +0100

    Render a score label in the top right corner

--- 08-SwiftyWords/SwiftyWords/ViewController.swift
+++ 08-SwiftyWords/SwiftyWords/ViewController.swift
@@ -9,6 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+  var cluesLabel: UILabel!
+  var answersLabel: UILabel!
+  var currentAnswer: UITextField!
+  var scoreLabel: UILabel!
+  var letterButtons = [UIButton]()
+
+  override func loadView() {
+    view = UIView()
+    view.backgroundColor = .white
+
+    scoreLabel = UILabel()
+    scoreLabel.translatesAutoresizingMaskIntoConstraints = false
+    scoreLabel.textAlignment = .right
+    scoreLabel.text = "Score: 0"
+    view.addSubview(scoreLabel)
+
+    NSLayoutConstraint.activate([
+      scoreLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
+      scoreLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor)
+    ])
+  }
 
   override func viewDidLoad() {
     super.viewDidLoad()
