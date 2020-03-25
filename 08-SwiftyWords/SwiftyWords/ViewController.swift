044e70e975162a604af995c0310272a3dc327325
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-03-25 08:42:28 +0100

    Render current answer text field

--- 08-SwiftyWords/SwiftyWords/ViewController.swift
+++ 08-SwiftyWords/SwiftyWords/ViewController.swift
@@ -36,8 +36,17 @@ class ViewController: UIViewController {
     answersLabel.translatesAutoresizingMaskIntoConstraints = false
     answersLabel.font = UIFont.systemFont(ofSize: 24)
     answersLabel.text = "ANSWERS"
+    answersLabel.textAlignment = .right
     answersLabel.numberOfLines = 0
     view.addSubview(answersLabel)
+
+    currentAnswer = UITextField()
+    currentAnswer.translatesAutoresizingMaskIntoConstraints = false
+    currentAnswer.placeholder = "Tap letters to guess"
+    currentAnswer.textAlignment = .center
+    currentAnswer.font = UIFont.systemFont(ofSize: 44)
+    currentAnswer.isUserInteractionEnabled = false
+    view.addSubview(currentAnswer)
 
     NSLayoutConstraint.activate([
       scoreLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor),
@@ -50,7 +59,11 @@ class ViewController: UIViewController {
       answersLabel.topAnchor.constraint(equalTo: scoreLabel.bottomAnchor),
       answersLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor, constant: -100),
       answersLabel.widthAnchor.constraint(equalTo: view.layoutMarginsGuide.widthAnchor, multiplier: 0.4, constant: -100),
-      answersLabel.heightAnchor.constraint(equalTo: cluesLabel.heightAnchor)
+      answersLabel.heightAnchor.constraint(equalTo: cluesLabel.heightAnchor),
+
+      currentAnswer.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      currentAnswer.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.5),
+      currentAnswer.topAnchor.constraint(equalTo: cluesLabel.bottomAnchor, constant: 20)
     ])
 
     cluesLabel.backgroundColor = .red
