5f411364daf39b9ebeff2625cde3d1bc219db8a9
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-03-27 09:08:13 +0100

    Copy Project 7 to Project 9

---
+++ 09-WhiteHousePetitions/README.md
@@ -0,0 +1,6 @@
+# Whitehouse Petitions
+
+The 9th project from 100 Days of Swift. More info [here](https://www.hackingwithswift.com/read/9/1/setting-up).
+
+Improved version of Project #7. Uses Grand Central Dispatch
+
---
+++ 09-WhiteHousePetitions/WhitehousePetitions/ViewController.swift
@@ -0,0 +1,80 @@
+//
+//  ViewController.swift
+//  WhitehousePetitions
+//
+//  Created by Vitali Tatarintev on 24.03.20.
+//  Copyright © 2020 Vitali Tatarintev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+  var petitions = [Petition]()
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    let urlString: String
+
+    navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Credits", style: .plain, target: self, action: #selector(showCredits))
+
+    if navigationController?.tabBarItem.tag == 0 {
+      urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+    } else {
+      urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+    }
+
+    if let url = URL(string: urlString) {
+      if let data = try? Data(contentsOf: url) {
+        parse(json: data)
+        return
+      }
+    }
+
+    showError()
+  }
+
+  func parse(json: Data) {
+    let decoder = JSONDecoder()
+
+    if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+      petitions = jsonPetitions.results
+      tableView.reloadData()
+    }
+  }
+
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    petitions.count
+  }
+
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    let petition = petitions[indexPath.row]
+    cell.textLabel?.text = petition.title
+    cell.detailTextLabel?.text = petition.title
+
+    return cell
+  }
+
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    let vc = DetailViewController()
+    vc.detailItem = petitions[indexPath.row]
+    navigationController?.pushViewController(vc, animated: true)
+  }
+
+  func showError() {
+    presentAlert(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again")
+  }
+
+  @objc func showCredits() {
+    presentAlert(title: "Credits", message: "All the data comes from the We The People API of the Whitehouse")
+  }
+
+  func presentAlert(title: String, message: String) {
+    let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    ac.addAction(UIAlertAction(title: "OK", style: .default))
+
+    present(ac, animated: true)
+  }
+}
+
