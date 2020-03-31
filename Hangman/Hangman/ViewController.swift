9eb0a144a04bbdb370bd26fb52effd3f890132da
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-03-31 10:16:49 +0200

    Render keyboard buttons

--- Hangman/Hangman/ViewController.swift
+++ Hangman/Hangman/ViewController.swift
@@ -22,17 +22,81 @@ class ViewController: UIViewController {
     keyboardView.backgroundColor = .systemTeal
     view.addSubview(keyboardView)
 
+    let keyboardRow1View = UIView()
+    keyboardRow1View.translatesAutoresizingMaskIntoConstraints = false
+    keyboardRow1View.backgroundColor = .systemPink
+    keyboardView.addSubview(keyboardRow1View)
+
+    let keyboardRow2View = UIView()
+    keyboardRow2View.translatesAutoresizingMaskIntoConstraints = false
+    keyboardRow2View.backgroundColor = .systemPink
+    keyboardView.addSubview(keyboardRow2View)
+
+    let keyboardRow3View = UIView()
+    keyboardRow3View.translatesAutoresizingMaskIntoConstraints = false
+    keyboardRow3View.backgroundColor = .systemPink
+    keyboardView.addSubview(keyboardRow3View)
+
+    let keyboardPadding: CGFloat = 5
+    let rowHeight: CGFloat = 60
+
     NSLayoutConstraint.activate([
       keyboardView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor, constant: -10),
-      keyboardView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor, constant: 5),
-      keyboardView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor, constant: -5),
-      keyboardView.heightAnchor.constraint(greaterThanOrEqualToConstant: 200)
+      keyboardView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor, constant: keyboardPadding),
+      keyboardView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor, constant: -keyboardPadding),
+      keyboardView.heightAnchor.constraint(greaterThanOrEqualToConstant: 200),
+
+      keyboardRow3View.bottomAnchor.constraint(equalTo: keyboardView.bottomAnchor, constant: -keyboardPadding),
+      keyboardRow3View.leadingAnchor.constraint(equalTo: keyboardView.leadingAnchor, constant: keyboardPadding),
+      keyboardRow3View.trailingAnchor.constraint(equalTo: keyboardView.trailingAnchor, constant: -keyboardPadding),
+      keyboardRow3View.heightAnchor.constraint(greaterThanOrEqualToConstant: rowHeight),
+
+      keyboardRow2View.bottomAnchor.constraint(equalTo: keyboardRow3View.topAnchor, constant: -keyboardPadding),
+      keyboardRow2View.leadingAnchor.constraint(equalTo: keyboardView.leadingAnchor, constant: keyboardPadding),
+      keyboardRow2View.trailingAnchor.constraint(equalTo: keyboardView.trailingAnchor, constant: -keyboardPadding),
+      keyboardRow2View.heightAnchor.constraint(greaterThanOrEqualToConstant: rowHeight),
+
+      keyboardRow1View.bottomAnchor.constraint(equalTo: keyboardRow2View.topAnchor, constant: -keyboardPadding),
+      keyboardRow1View.leadingAnchor.constraint(equalTo: keyboardView.leadingAnchor, constant: keyboardPadding),
+      keyboardRow1View.trailingAnchor.constraint(equalTo: keyboardView.trailingAnchor, constant: -keyboardPadding),
+      keyboardRow1View.heightAnchor.constraint(greaterThanOrEqualToConstant: rowHeight)
     ])
+
+    addKeysToKeyboardRow(keys: keyboardRow1, rowView: keyboardRow1View, leadingPadding: 5)
+    addKeysToKeyboardRow(keys: keyboardRow2, rowView: keyboardRow2View, leadingPadding: 22)
+    addKeysToKeyboardRow(keys: keyboardRow3, rowView: keyboardRow3View, leadingPadding: 56)
+  }
+
+  func addKeysToKeyboardRow(keys: [String], rowView: UIView, leadingPadding: CGFloat = 5) {
+    let buttonWidth: CGFloat = 30
+    var leadingAnchor = rowView.leadingAnchor
+    var firstButton = true
+
+    for button in keys {
+      let keyButton = UIButton(type: .system)
+      keyButton.translatesAutoresizingMaskIntoConstraints = false
+      keyButton.setTitle(button, for: .normal)
+      keyButton.backgroundColor = .systemGray3
+      keyButton.setTitleColor(.white, for: .normal)
+
+      rowView.addSubview(keyButton)
+
+      NSLayoutConstraint.activate([
+        keyButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: firstButton ? leadingPadding : 5),
+        keyButton.topAnchor.constraint(equalTo: rowView.topAnchor, constant: 5),
+        keyButton.bottomAnchor.constraint(equalTo: rowView.bottomAnchor, constant: -5),
+        keyButton.widthAnchor.constraint(equalToConstant: buttonWidth)
+      ])
+
+      firstButton = false
+      leadingAnchor = keyButton.trailingAnchor
+    }
   }
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+
   }
 
 
