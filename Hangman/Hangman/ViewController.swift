3721b96e5e808b86b13bb466525631a00bf72e5f
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-03-31 08:29:23 +0200

    Add a placeholder for keyboard view

--- Hangman/Hangman/ViewController.swift
+++ Hangman/Hangman/ViewController.swift
@@ -9,6 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+  let keyboardRow1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"]
+  let keyboardRow2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"]
+  let keyboardRow3 = ["Z", "X", "C", "V", "B", "N", "M"]
+
+  override func loadView() {
+    view = UIView()
+    view.backgroundColor = .white
+
+    let keyboardView = UIView()
+    keyboardView.translatesAutoresizingMaskIntoConstraints = false
+    keyboardView.backgroundColor = .systemTeal
+    view.addSubview(keyboardView)
+
+    NSLayoutConstraint.activate([
+      keyboardView.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor, constant: -10),
+      keyboardView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor, constant: 5),
+      keyboardView.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor, constant: -5),
+      keyboardView.heightAnchor.constraint(greaterThanOrEqualToConstant: 200)
+    ])
+  }
 
   override func viewDidLoad() {
     super.viewDidLoad()
