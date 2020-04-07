fb863d607fd670ecef9acf97c05b7d8d6cd272d2
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-04-07 10:27:24 +0200

    Run a login mutation on tapping submit button

--- RocketReserver/RocketReserver/LoginViewController.swift
+++ RocketReserver/RocketReserver/LoginViewController.swift
@@ -15,12 +15,45 @@ class LoginViewController: UIViewController {
   @IBOutlet private var submitButton: UIButton!
 
   @IBAction private func submitTapped() {
+    self.enableSubmitButton(false)
 
+    guard let email = self.emailTextField.text else {
+      self.errorLabel.text = "Please enter an email address."
+      self.enableSubmitButton(true)
+      return
+    }
+
+    guard self.validate(email: email) else {
+      self.errorLabel.text = "Please enter a valid email."
+      self.enableSubmitButton(true)
+      return
+    }
+
+    Network.shared.apollo.perform(mutation: LoginMutation(email: email)) { [weak self] result in
+      defer { self?.enableSubmitButton(true) }
+
+      switch result {
+      case .success(let graphQLResult):
+        if let token = graphQLResult.data?.login {
+          let keychain = KeychainSwift()
+          keychain.set(token, forKey: LoginViewController.loginKeychainKey)
+          self?.dismiss(animated: true)
+        }
+
+        if let errors = graphQLResult.errors {
+          print("Errors from server: \(errors)")
+        }
+      case .failure(let error):
+        print("Error: \(error)")
+      }
+    }
   }
 
   @IBAction private func cancelTapped() {
     self.dismiss(animated: true)
   }
+
+  static let loginKeychainKey = "login"
 
   override func viewDidLoad() {
     super.viewDidLoad()
