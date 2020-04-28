e053b6becc9c68c89291939b3572580686acc2a3
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-04-28 21:44:08 +0200

    Display 10 fake cells

--- 10-NamesToFaces/NamesToFaces/ViewController.swift
+++ 10-NamesToFaces/NamesToFaces/ViewController.swift
@@ -15,6 +15,17 @@ class ViewController: UICollectionViewController {
     // Do any additional setup after loading the view.
   }
 
+  override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 10
+  }
 
+  override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+      // we failed to get a PersonCell – bail out!
+      fatalError("Unable to dequeue PersonCell.")
+    }
+
+    return cell
+  }
 }
 
