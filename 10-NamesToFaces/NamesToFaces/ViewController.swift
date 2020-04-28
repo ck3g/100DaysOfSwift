e9bbc4852ffd57f5eaff0d6f2db777996cd08b5b
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-04-28 20:38:48 +0200

    Create a blank NameToFaces project

---
+++ 10-NamesToFaces/NamesToFaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NamesToFaces
+//
+//  Created by Vitali Tatarintev on 28.04.20.
+//  Copyright © 2020 Vitali Tatarintev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ 10-NamesToFaces/README.md
@@ -0,0 +1,6 @@
+# Names to Faces
+
+A project #10 from 100 Days of Swift.
+
+More info [here](https://www.hackingwithswift.com/100/42).
+
