e8add1aab8508b90c16f2ce7a067be1588d68566
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-04-29 08:35:53 +0200

    Add sourceType = .camera if that's possible

--- 10-NamesToFaces/NamesToFaces/ViewController.swift
+++ 10-NamesToFaces/NamesToFaces/ViewController.swift
@@ -46,6 +46,11 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     let picker = UIImagePickerController()
     picker.allowsEditing = true
     picker.delegate = self
+
+    if UIImagePickerController.isSourceTypeAvailable(.camera) {
+      picker.sourceType = .camera
+    }
+
     present(picker, animated: true)
   }
 
