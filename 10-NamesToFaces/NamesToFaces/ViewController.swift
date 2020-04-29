eb6e4461e5b592b8ad203d6dfa4e1963d5416160
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-04-29 08:16:57 +0200

    Allow to change picture label

--- 10-NamesToFaces/NamesToFaces/ViewController.swift
+++ 10-NamesToFaces/NamesToFaces/ViewController.swift
@@ -70,4 +70,22 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
     return paths[0]
   }
+
+  override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    let person = people[indexPath.item]
+
+    let ac = UIAlertController(title: "Rename person", message: nil, preferredStyle: .alert)
+    ac.addTextField()
+
+    ac.addAction(UIAlertAction(title: "OK", style: .default) { [weak self, weak ac] _ in
+      guard let newName = ac?.textFields?[0].text else { return }
+
+      person.name = newName
+      self?.collectionView.reloadData()
+    })
+
+    ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+
+    present(ac, animated: true)
+  }
 }
