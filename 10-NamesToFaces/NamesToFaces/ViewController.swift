a8dfe9e61d6a0a92404fbe5338add02341de753a
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-04-29 08:04:53 +0200

    Append a person to the list of people

--- 10-NamesToFaces/NamesToFaces/ViewController.swift
+++ 10-NamesToFaces/NamesToFaces/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+  var people = [Person]()
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -45,6 +46,10 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     if let jpegData = image.jpegData(compressionQuality: 0.8) {
       try? jpegData.write(to: imagePath)
     }
+
+    let person = Person(name: "Unknown", image: imageName)
+    people.append(person)
+    collectionView.reloadData()
 
     dismiss(animated: true)
   }
