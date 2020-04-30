954843e6c95aa59259a7f937d3d903e70fe5af8f
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-04-30 14:57:51 +0200

    Make box falling

--- 11-Pachinko/Pachinko/GameScene.swift
+++ 11-Pachinko/Pachinko/GameScene.swift
@@ -13,7 +13,21 @@ class GameScene: SKScene {
   override func didMove(to view: SKView) {
     let background = SKSpriteNode(imageNamed: "background")
     background.position = CGPoint(x: 512, y: 384)
+    background.blendMode = .replace // renders faster
+    background.zPosition = -1
     addChild(background)
+
+    physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+  }
+
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    guard let touch = touches.first else { return }
+    let location = touch.location(in: self)
+
+    let box = SKSpriteNode(color: .red, size: CGSize(width: 64, height: 64))
+    box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
+    box.position = location
+    addChild(box)
   }
   
 }
