9f364acf9ff13dcb3052c23c6c3cb3f0a6e6a473
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-04-30 14:50:43 +0200

    Set the background

--- 11-Pachinko/Pachinko/GameScene.swift
+++ 11-Pachinko/Pachinko/GameScene.swift
@@ -11,8 +11,9 @@ import SpriteKit
 class GameScene: SKScene {
   
   override func didMove(to view: SKView) {
-    
-    
+    let background = SKSpriteNode(imageNamed: "background")
+    background.position = CGPoint(x: 512, y: 384)
+    addChild(background)
   }
   
 }
