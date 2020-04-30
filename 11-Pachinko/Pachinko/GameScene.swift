8c85369188692a7ec9504fc25fa7bca70be0642b
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-04-30 15:03:39 +0200

    Add a bouncer to the center of the screen

--- 11-Pachinko/Pachinko/GameScene.swift
+++ 11-Pachinko/Pachinko/GameScene.swift
@@ -18,6 +18,12 @@ class GameScene: SKScene {
     addChild(background)
 
     physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+
+    let bouncer = SKSpriteNode(imageNamed: "bouncer")
+    bouncer.position = CGPoint(x: 512, y: 0)
+    bouncer.physicsBody = SKPhysicsBody(circleOfRadius: bouncer.size.width / 2)
+    bouncer.physicsBody?.isDynamic = false
+    addChild(bouncer)
   }
 
   override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
