727b5fda3eed61c9e881d29f2792ae63243bfdbe
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-05-03 07:39:17 +0200

    Add explode effect when a ball disappears

--- 11-Pachinko/Pachinko/GameScene.swift
+++ 11-Pachinko/Pachinko/GameScene.swift
@@ -143,6 +143,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
   }
 
   func destroy(ball: SKNode) {
+    if let fireParticles = SKEmitterNode(fileNamed: "FireParticles") {
+      fireParticles.position = ball.position
+      addChild(fireParticles)
+    }
+    
     ball.removeFromParent()
   }
 
