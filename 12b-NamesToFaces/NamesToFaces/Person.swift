5feb84e1a34a57418af12ec680d6981e00eec724
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-05-04 08:44:03 +0200

    Create two extra copies of Project 10

---
+++ 12b-NamesToFaces/NamesToFaces/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  NamesToFaces
+//
+//  Created by Vitali Tatarintev on 29.04.20.
+//  Copyright © 2020 Vitali Tatarintev. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+  var name: String
+  var image: String
+
+  init(name: String, image: String) {
+    self.name = name
+    self.image = image
+  }
+}
---
+++ 12b-NamesToFaces/NamesToFaces/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  NamesToFaces
+//
+//  Created by Vitali Tatarintev on 28.04.20.
+//  Copyright © 2020 Vitali Tatarintev. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+  @IBOutlet var imageView: UIImageView!
+  @IBOutlet var name: UILabel!
+  
+}
