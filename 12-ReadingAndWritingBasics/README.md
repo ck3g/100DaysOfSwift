a8f127e482edd74b5cb58b523f6a8dc1983c06d8
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-05-04 08:48:12 +0200

    Create blank Reading and Writing basics project

---
+++ 12-ReadingAndWritingBasics/README.md
@@ -0,0 +1,5 @@
+# Reading and Writing Basics
+
+Playground project of 100 Days of Swift
+
+More info [here](https://www.hackingwithswift.com/read/12/2/reading-and-writing-basics-userdefaults).
---
+++ 12-ReadingAndWritingBasics/ReadingAndWritingBasics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReadingAndWritingBasics
+//
+//  Created by Vitali Tatarintev on 04.05.20.
+//  Copyright © 2020 Vitali Tatarintev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
