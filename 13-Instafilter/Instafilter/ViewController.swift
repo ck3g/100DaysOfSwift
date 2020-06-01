f6187f739d4a35abce6518bcd7c598f1993af326
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-06-01 09:42:17 +0200

    Create a blank Instafilter project

---
+++ 13-Instafilter/Instafilter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instafilter
+//
+//  Created by Vitali Tatarintev on 01.06.20.
+//  Copyright © 2020 Vitali Tatarintev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ 13-Instafilter/README.md
@@ -0,0 +1,3 @@
+# Instafilter
+
+The project from 100 Days of Swift. More info [here](https://www.hackingwithswift.com/100/52).
