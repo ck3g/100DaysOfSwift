74c71ad2f42939064cec36b1ab6ff392db1706bc
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-06-02 09:49:56 +0200

    Add color transform animation

--- 15-AnimationSandbox/AnimationSandbox/ViewController.swift
+++ 15-AnimationSandbox/AnimationSandbox/ViewController.swift
@@ -38,6 +38,12 @@ class ViewController: UIViewController {
         self.imageView.transform = CGAffineTransform(rotationAngle: .pi)
       case 5:
         self.imageView.transform = .identity
+      case 6:
+        self.imageView.alpha = 0.1
+        self.imageView.backgroundColor = .green
+      case 7:
+        self.imageView.alpha = 1
+        self.imageView.backgroundColor = .clear
       default:
         break
       }
