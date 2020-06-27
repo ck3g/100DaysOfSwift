ccfb660aaf5faab0ffb48d1473d61ae2e4d3e393
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-06-27 17:40:59 +0200

    Create a blank Debugging project

---
+++ 18-Debugging/Debugging/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Debugging
+//
+//  Created by Vitali Tatarintev on 27.06.20.
+//  Copyright © 2020 Vitali Tatarintev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ 18-Debugging/README.md
@@ -0,0 +1,5 @@
+# Debugging - a technique project
+
+
+Project 18 from 100 Days of Swift.
+More info [here](https://www.hackingwithswift.com/100/64).
