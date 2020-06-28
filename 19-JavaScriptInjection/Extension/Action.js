7701a083e8564bf5945097b0548f657f83f35d92
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-06-28 17:08:38 +0200

    Create an extension code structure

---
+++ 19-JavaScriptInjection/Extension/Action.js
@@ -0,0 +1,12 @@
+var action = function() {}
+
+Action.prototype = {
+  run: function(parameters) {
+
+  },
+  finalize: function(parameters) {
+
+  }
+};
+
+var ExtenstionPreprocessingJS = new Action
--- 19-JavaScriptInjection/Extension/ActionViewController.swift
+++ 19-JavaScriptInjection/Extension/ActionViewController.swift
@@ -16,36 +16,13 @@ class ActionViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     
-        // Get the item[s] we're handling from the extension context.
-        
-        // For example, look for an image and place it into an image view.
-        // Replace this with something appropriate for the type[s] your extension supports.
-        var imageFound = false
-        for item in self.extensionContext!.inputItems as! [NSExtensionItem] {
-            for provider in item.attachments! {
-                if provider.hasItemConformingToTypeIdentifier(kUTTypeImage as String) {
-                    // This is an image. We'll load it, then place it in our image view.
-                    weak var weakImageView = self.imageView
-                    provider.loadItem(forTypeIdentifier: kUTTypeImage as String, options: nil, completionHandler: { (imageURL, error) in
-                        OperationQueue.main.addOperation {
-                            if let strongImageView = weakImageView {
-                                if let imageURL = imageURL as? URL {
-                                    strongImageView.image = UIImage(data: try! Data(contentsOf: imageURL))
-                                }
-                            }
-                        }
-                    })
-                    
-                    imageFound = true
-                    break
-                }
-            }
-            
-            if (imageFound) {
-                // We only handle one image, so stop looking for more.
-                break
-            }
+      if let inputItem = extensionContext?.inputItems.first as? NSExtensionItem {
+        if let itemProvider = inputItem.attachments?.first {
+          itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) { [weak self] (dict, error) in
+            // so some work here
+          }
         }
+      }
     }
 
     @IBAction func done() {
