1d5e92b9f8c665b4c77857cbc802674dbf7b3b00
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-06-29 08:50:21 +0200

    Make keyboard to not overload the text view

--- 19-JavaScriptInjection/Extension/ActionViewController.swift
+++ 19-JavaScriptInjection/Extension/ActionViewController.swift
@@ -19,6 +19,10 @@ class ActionViewController: UIViewController {
     super.viewDidLoad()
 
     navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+
+    let notificationCenter = NotificationCenter.default
+    notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+    notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
     
     if let inputItem = extensionContext?.inputItems.first as? NSExtensionItem {
       if let itemProvider = inputItem.attachments?.first {
@@ -44,6 +48,24 @@ class ActionViewController: UIViewController {
     let customJavaScript = NSItemProvider(item: webDictionary, typeIdentifier: kUTTypePropertyList as String)
     item.attachments = [customJavaScript]
     extensionContext?.completeRequest(returningItems: [item])
+  }
+
+  @objc func adjustForKeyboard(notification: Notification) {
+    guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+
+    let keyboardScreenEndFrame = keyboardValue.cgRectValue
+    let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+
+    if notification.name == UIResponder.keyboardWillHideNotification {
+      script.contentInset = .zero
+    } else {
+      script.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height, right: 0)
+    }
+
+    script.scrollIndicatorInsets = script.contentInset
+
+    let selectedRange = script.selectedRange
+    script.scrollRangeToVisible(selectedRange)
   }
 
 }
