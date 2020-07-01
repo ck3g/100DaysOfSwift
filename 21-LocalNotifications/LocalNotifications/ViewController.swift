17a6f04ab6312dcdd8e583775a5a2240c39accbf
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-07-01 10:30:33 +0200

    Register and schedule notifications

--- 21-LocalNotifications/LocalNotifications/ViewController.swift
+++ 21-LocalNotifications/LocalNotifications/ViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 Vitali Tatarintev. All rights reserved.
 //
 
+import UserNotifications
 import UIKit
 
 class ViewController: UIViewController {
@@ -18,11 +19,39 @@ class ViewController: UIViewController {
   }
 
   @objc func registerLocal() {
+    let center = UNUserNotificationCenter.current()
 
+    center.requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+      if granted {
+        print("Yay!")
+      } else {
+        print("D'oh!")
+      }
+    }
   }
 
   @objc func scheduleLocal() {
+    let center = UNUserNotificationCenter.current()
+    center.removeAllPendingNotificationRequests()
 
+    let content = UNMutableNotificationContent()
+    content.title = "Late wake up call"
+    content.body = "The early bird catches the worm, but the second mouse gets the cheese."
+    content.categoryIdentifier = "alarm"
+    content.userInfo = ["customData": "fizzbuzz"]
+    content.sound = .default
+
+    var dateComponents = DateComponents()
+    dateComponents.hour = 10
+    dateComponents.minute = 30
+    // Triggers for the specific time
+    // let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+
+    // Triggers with the time interval
+    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+
+    let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+    center.add(request)
   }
 }
 
