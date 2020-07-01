bdff6161154e2bdb70214082e2abe8fb20cef811
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-07-01 10:17:38 +0200

    Add nav bar with two bar item buttons

--- 21-LocalNotifications/LocalNotifications/ViewController.swift
+++ 21-LocalNotifications/LocalNotifications/ViewController.swift
@@ -12,9 +12,17 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+
+    navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(registerLocal))
+    navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self, action: #selector(scheduleLocal))
   }
 
+  @objc func registerLocal() {
 
+  }
+
+  @objc func scheduleLocal() {
+
+  }
 }
 
