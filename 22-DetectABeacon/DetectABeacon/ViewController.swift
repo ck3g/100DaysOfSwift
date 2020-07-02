c4805a7153f6a6acc47182501e3f018cc7ff425c
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-07-02 10:15:07 +0200

    Update the UI depending on the distance to a beacon

--- 22-DetectABeacon/DetectABeacon/ViewController.swift
+++ 22-DetectABeacon/DetectABeacon/ViewController.swift
@@ -42,5 +42,35 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     locationManager?.startMonitoring(for: beaconRegion)
     locationManager?.startRangingBeacons(in: beaconRegion)
   }
+
+  func update(distance: CLProximity) {
+    UIView.animate(withDuration: 1) {
+      switch distance {
+      case .far:
+        self.view.backgroundColor = .blue
+        self.distanceReading.text = "FAR"
+
+      case .near:
+        self.view.backgroundColor = .orange
+        self.distanceReading.text = "NEAR"
+
+      case .immediate:
+        self.view.backgroundColor = .red
+        self.distanceReading.text = "RIGHT HERE"
+
+      default:
+        self.view.backgroundColor = .gray
+        self.distanceReading.text = "UNKNOWN"
+      }
+    }
+  }
+
+  func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
+    if let beacon = beacons.first {
+      update(distance: beacon.proximity)
+    } else {
+      update(distance: .unknown)
+    }
+  }
 }
 
