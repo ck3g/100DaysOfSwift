7504fbddb22e1f4069d79f10eb22f1734a9acac4
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-07-02 10:00:43 +0200

    Build the UI and request the location usage

--- 22-DetectABeacon/DetectABeacon/ViewController.swift
+++ 22-DetectABeacon/DetectABeacon/ViewController.swift
@@ -6,15 +6,33 @@
 //  Copyright © 2020 Vitali Tatarintev. All rights reserved.
 //
 
+import CoreLocation
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+  @IBOutlet var distanceReading: UILabel!
+
+  var locationManager: CLLocationManager?
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+
+    locationManager = CLLocationManager()
+    locationManager?.delegate = self
+    locationManager?.requestAlwaysAuthorization()
+    // locationManager?.requestWhenInUseAuthorization() // Alternatively
+
+    view.backgroundColor = .gray
   }
 
-
+  func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+    if status == .authorizedAlways {
+      if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+        if CLLocationManager.isRangingAvailable() {
+          // do stuff
+        }
+      }
+    }
+  }
 }
 
