d6422d2e5e7a09e130df592e0b4b984b8050f949
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-07-02 09:47:39 +0200

    Create a blank Detect a Beacon project

---
+++ 22-DetectABeacon/DetectABeacon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DetectABeacon
+//
+//  Created by Vitali Tatarintev on 02.07.20.
+//  Copyright © 2020 Vitali Tatarintev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ 22-DetectABeacon/README.md
@@ -0,0 +1,4 @@
+# Detect a Beacon
+
+Project 22 from 100 Days of Swift.
+More info [here](https://www.hackingwithswift.com/100/75).
