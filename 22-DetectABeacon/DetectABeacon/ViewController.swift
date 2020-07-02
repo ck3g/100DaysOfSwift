a85319cbf3a42d5b70341f214e7fdc731b775d22
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-07-02 10:08:04 +0200

    Start scanning for a beacon

--- 22-DetectABeacon/DetectABeacon/ViewController.swift
+++ 22-DetectABeacon/DetectABeacon/ViewController.swift
@@ -29,10 +29,18 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     if status == .authorizedAlways {
       if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
         if CLLocationManager.isRangingAvailable() {
-          // do stuff
+          startScanning()
         }
       }
     }
+  }
+
+  func startScanning() {
+    let uuid = UUID(uuidString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5")!
+    let beaconRegion = CLBeaconRegion(uuid: uuid, major: 123, minor: 456, identifier: "MyBeacon")
+
+    locationManager?.startMonitoring(for: beaconRegion)
+    locationManager?.startRangingBeacons(in: beaconRegion)
   }
 }
 
