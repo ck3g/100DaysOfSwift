6a5c6eced060612a07df1416842c57e098000a35
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-07-03 22:00:17 +0200

    Destroy the enemy

--- 23-SwiftyNinja/SwiftyNinja/GameScene.swift
+++ 23-SwiftyNinja/SwiftyNinja/GameScene.swift
@@ -115,6 +115,63 @@ class GameScene: SKScene {
     if !isSwooshSoundActive {
       playSwooshSound()
     }
+
+    let nodesAtPoint = nodes(at: location)
+
+    for case let node as SKSpriteNode in nodesAtPoint {
+      if node.name == "enemy" {
+        // destroy the penguin
+        if let emitter = SKEmitterNode(fileNamed: "sliceHitEnemy") {
+          emitter.position = node.position
+          addChild(emitter)
+        }
+
+        node.name = ""
+        node.physicsBody?.isDynamic = false
+
+        let scaleOut = SKAction.scale(to: 0.001, duration: 0.2)
+        let fadeOut = SKAction.fadeOut(withDuration: 0.2)
+        let group = SKAction.group([scaleOut, fadeOut])
+        let seq = SKAction.sequence([group, .removeFromParent()])
+        node.run(seq)
+
+        score += 1
+
+        if let index = activeEnemies.firstIndex(of: node) {
+          activeEnemies.remove(at: index)
+        }
+
+        run(SKAction.playSoundFileNamed("whack.caf", waitForCompletion: false))
+      } else if node.name == "bomb" {
+        // destroy the bomb
+        guard let bombContainer = node.parent as? SKSpriteNode else { continue }
+
+        if let emitter = SKEmitterNode(fileNamed: "sliceHitBomb") {
+          emitter.position = bombContainer.position
+          addChild(emitter)
+        }
+
+        node.name = ""
+        bombContainer.physicsBody?.isDynamic = false
+
+        let scaleOut = SKAction.scale(to: 0.001, duration: 0.2)
+        let fadeOut = SKAction.fadeOut(withDuration: 0.2)
+        let group = SKAction.group([scaleOut, fadeOut])
+        let seq = SKAction.sequence([group, .removeFromParent()])
+        bombContainer.run(seq)
+
+        if let index = activeEnemies.firstIndex(of: bombContainer) {
+          activeEnemies.remove(at: index)
+        }
+
+        run(SKAction.playSoundFileNamed("explosion.caf", waitForCompletion: false))
+        endGame()
+      }
+    }
+  }
+
+  func endGame() {
+
   }
 
   func playSwooshSound() {
@@ -243,13 +300,27 @@ class GameScene: SKScene {
     activeEnemies.append(enemy)
   }
 
+  func subtractLife() {
+
+  }
+
   override func update(_ currentTime: TimeInterval) {
     if activeEnemies.count > 0 {
       for (index, node) in activeEnemies.enumerated().reversed() {
         if node.position.y < -140 {
-          node.removeFromParent()
+          node.removeAllActions()
 
-          activeEnemies.remove(at: index)
+          if node.name == "enemy" {
+            node.name = ""
+            subtractLife()
+
+            node.removeFromParent()
+            activeEnemies.remove(at: index)
+          } else if node.name == "bombContainer" {
+            node.name = ""
+            node.removeFromParent()
+            activeEnemies.remove(at: index)
+          }
         }
       }
     } else {
