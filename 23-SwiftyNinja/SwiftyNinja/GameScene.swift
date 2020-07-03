d6194eacc902f4bdbff6a22ca7bc1dfb8da6982a
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-07-03 20:12:27 +0200

    Add a background and define a program structure

--- 23-SwiftyNinja/SwiftyNinja/GameScene.swift
+++ 23-SwiftyNinja/SwiftyNinja/GameScene.swift
@@ -9,8 +9,41 @@
 import SpriteKit
 
 class GameScene: SKScene {
+  var gameScore: SKLabelNode!
+
+  var score = 0 {
+    didSet {
+      gameScore.text = "Score: \(score)"
+    }
+  }
+
+  var liveImages = [SKSpriteNode]()
+  var lives = 3
 
   override func didMove(to view: SKView) {
+    let background = SKSpriteNode(imageNamed: "sliceBackground")
+    background.position = CGPoint(x: 512, y: 384)
+    background.blendMode = .replace
+    background.zPosition = -1
+    addChild(background)
+
+    physicsWorld.gravity = CGVector(dx: 0, dy: -6)
+    physicsWorld.speed = 0.85
+
+    createScore()
+    createLives()
+    createSlices()
+  }
+
+  func createScore() {
+
+  }
+
+  func createLives() {
+
+  }
+
+  func createSlices() {
 
   }
 
