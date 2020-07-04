92baab6e82f9ee46c097d5640bc1c435d1895db2
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-07-04 18:09:30 +0200

    More about strings

--- 24-Strings.playground/Contents.swift
+++ 24-Strings.playground/Contents.swift
@@ -23,3 +23,60 @@ extension String {
 
 // So now, that works
 let letter2 = name[3]
+
+
+// -------------
+
+let password = "12345"
+password.hasPrefix("123")
+password.hasSuffix("456")
+
+extension String {
+  func deletingPrefix(_ prefix: String) -> String {
+    guard self.hasPrefix(prefix) else { return self }
+    return String(self.dropFirst(prefix.count))
+  }
+
+  func deletingSuffix(_ suffix: String) -> String {
+    guard self.hasSuffix(suffix) else { return self }
+    return String(self.dropLast(suffix.count))
+  }
+}
+
+// ---------------
+
+let weather = "it's going to rain"
+print(weather.capitalized)
+
+extension String {
+  var capitalizedFirst: String {
+    guard let firstLetter = self.first else { return "" }
+    return firstLetter.uppercased() + self.dropFirst()
+  }
+}
+
+print(weather.capitalizedFirst)
+
+// ----------------
+
+let input = "Swift is like Objective-C without the C"
+input.contains("Swift")
+
+let languages = ["Python", "Ruby", "Swift"]
+languages.contains("Swift")
+
+extension String {
+  func containsAny(of array: [String]) -> Bool {
+    for item in array {
+      if self.contains(item) {
+        return true
+      }
+    }
+
+    return false
+  }
+}
+
+input.containsAny(of: languages) // <- but that's not elegant
+
+languages.contains(where: input.contains) // <- more elegant solution
