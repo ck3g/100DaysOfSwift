6947119e038f77c529c7243ebc885e1e7ffede07
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-07-04 18:19:05 +0200

    Formatting strings

--- 24-Strings.playground/Contents.swift
+++ 24-Strings.playground/Contents.swift
@@ -80,3 +80,22 @@ extension String {
 input.containsAny(of: languages) // <- but that's not elegant
 
 languages.contains(where: input.contains) // <- more elegant solution
+
+// ----------------------
+// Formatting strings
+
+let string = "This is a test string"
+let attributes: [NSAttributedString.Key: Any] = [
+  .foregroundColor: UIColor.white,
+  .backgroundColor: UIColor.red,
+  .font: UIFont.boldSystemFont(ofSize: 36)
+]
+let atributtedString = NSAttributedString(string: string, attributes: attributes)
+
+let attributesString2 = NSMutableAttributedString(string: string)
+
+attributesString2.addAttribute(.font, value: UIFont.systemFont(ofSize: 8), range: NSRange(location: 0, length: 4))
+attributesString2.addAttribute(.font, value: UIFont.systemFont(ofSize: 16), range: NSRange(location: 5, length: 2))
+attributesString2.addAttribute(.font, value: UIFont.systemFont(ofSize: 24), range: NSRange(location: 8, length: 1))
+attributesString2.addAttribute(.font, value: UIFont.systemFont(ofSize: 32), range: NSRange(location: 10, length: 4))
+attributesString2.addAttribute(.font, value: UIFont.systemFont(ofSize: 40), range: NSRange(location: 15, length: 6))
