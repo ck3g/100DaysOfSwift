4526f0193d52fae4ce0f53594fd7cc5b1432c0c5
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-07-04 17:55:51 +0200

    Strings are not arrays

--- 24-Strings.playground/Contents.swift
+++ 24-Strings.playground/Contents.swift
@@ -1,3 +1,25 @@
 import UIKit
 
-var str = "Hello, playground"
+// Strings are not arrays
+let name = "Taylor"
+
+// Sure we can iterate through them
+for letter in name {
+  print("Give me a \(letter)!")
+}
+
+// But we can't get a letter by index
+// print(name[3]) // does not compile
+
+// We access a letter by index like that
+let letter = name[name.index(name.startIndex, offsetBy: 3)]
+
+// We can also make an extension
+extension String {
+  subscript(i: Int) -> String {
+    return String(self[index(startIndex, offsetBy: i)])
+  }
+}
+
+// So now, that works
+let letter2 = name[3]
--- 24-Strings.playground/README.md
+++
@@ -1,4 +0,0 @@
-# Strings
-
-Project 24 from 100 Days of Swift.
-More info [here](https://www.hackingwithswift.com/100/80).
