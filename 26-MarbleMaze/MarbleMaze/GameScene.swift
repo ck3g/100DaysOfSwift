9ecb30079c50e3d2aa35e0518ef80899634eac5b
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-07-05 16:15:03 +0200

    Add a player to the game

--- 26-MarbleMaze/MarbleMaze/GameScene.swift
+++ 26-MarbleMaze/MarbleMaze/GameScene.swift
@@ -17,6 +17,7 @@ enum CollisionTypes: UInt32 {
 }
 
 class GameScene: SKScene {
+  var player: SKSpriteNode!
 
   override func didMove(to view: SKView) {
     let background = SKSpriteNode(imageNamed: "background")
@@ -26,6 +27,7 @@ class GameScene: SKScene {
     addChild(background)
     
     loadLevel()
+    createPlayer()
   }
 
   func loadLevel() {
@@ -95,5 +97,18 @@ class GameScene: SKScene {
         }
       }
     }
+  }
+
+  func createPlayer() {
+    player = SKSpriteNode(imageNamed: "player")
+    player.position = CGPoint(x: 96, y: 672)
+    player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.width / 2)
+    player.physicsBody?.allowsRotation = false
+    player.physicsBody?.linearDamping = 0.5
+
+    player.physicsBody?.categoryBitMask = CollisionTypes.player.rawValue
+    player.physicsBody?.contactTestBitMask = CollisionTypes.star.rawValue | CollisionTypes.vortex.rawValue | CollisionTypes.finish.rawValue
+    player.physicsBody?.collisionBitMask = CollisionTypes.wall.rawValue
+    addChild(player)
   }
 }
