be3f6af4ea13e12759a1008b3ee2b8ae3036da43
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-07-05 18:14:10 +0200

    Build a UI

--- 27-CoreGraphics/CoreGraphics/ViewController.swift
+++ 27-CoreGraphics/CoreGraphics/ViewController.swift
@@ -9,12 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+  @IBOutlet var imageView: UIImageView!
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
 
+  @IBAction func redrawTapped(_ sender: Any) {
+  }
 
 }
 
