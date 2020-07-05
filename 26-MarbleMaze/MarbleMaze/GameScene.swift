1bb5f205ba414871c1728d3aa3601a29cee8de3d
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-07-05 16:20:44 +0200

    Allow to move the player with touches

--- 26-MarbleMaze/MarbleMaze/GameScene.swift
+++ 26-MarbleMaze/MarbleMaze/GameScene.swift
@@ -18,6 +18,7 @@ enum CollisionTypes: UInt32 {
 
 class GameScene: SKScene {
   var player: SKSpriteNode!
+  var lastTouchPosition: CGPoint?
 
   override func didMove(to view: SKView) {
     let background = SKSpriteNode(imageNamed: "background")
@@ -112,5 +113,28 @@ class GameScene: SKScene {
     player.physicsBody?.contactTestBitMask = CollisionTypes.star.rawValue | CollisionTypes.vortex.rawValue | CollisionTypes.finish.rawValue
     player.physicsBody?.collisionBitMask = CollisionTypes.wall.rawValue
     addChild(player)
+  }
+
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    guard let touch = touches.first else { return }
+    let location = touch.location(in: self)
+    lastTouchPosition = location
+  }
+
+  override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    guard let touch = touches.first else { return }
+    let location = touch.location(in: self)
+    lastTouchPosition = location
+  }
+
+  override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    lastTouchPosition = nil
+  }
+
+  override func update(_ currentTime: TimeInterval) {
+    if let lastTouchPosition = lastTouchPosition {
+      let diff = CGPoint(x: lastTouchPosition.x - player.position.x, y: lastTouchPosition.y - player.position.y)
+      physicsWorld.gravity = CGVector(dx: diff.x / 100, dy: diff.y / 100)
+    }
   }
 }
