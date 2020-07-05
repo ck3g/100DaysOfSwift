42d34695872c1983142cd0cd40775a6ed911e06d
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-07-05 11:18:38 +0200

    Allow choosing a photo from the library

--- 25-SelfieShare/SelfieShare/ViewController.swift
+++ 25-SelfieShare/SelfieShare/ViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class ViewController: UICollectionViewController {
+class ViewController: UICollectionViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+  var images = [UIImage]()
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -17,8 +18,34 @@ class ViewController: UICollectionViewController {
     navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importPicture))
   }
 
+  override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return images.count
+  }
+
+  override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageView", for: indexPath)
+
+    if let imageView = cell.viewWithTag(1000) as? UIImageView {
+      imageView.image = images[indexPath.item]
+    }
+
+    return cell
+  }
+
   @objc func importPicture() {
-    
+    let picker = UIImagePickerController()
+    picker.allowsEditing = true
+    picker.delegate = self
+    present(picker, animated: true)
+  }
+
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    guard let image = info[.editedImage] as? UIImage else { return }
+
+    dismiss(animated: true)
+
+    images.insert(image, at: 0)
+    collectionView.reloadData()
   }
 
 }
