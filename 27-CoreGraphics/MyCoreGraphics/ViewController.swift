e6c1966cd1cd803ba1853cbbc313cd3077406fa0
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-07-05 18:46:30 +0200

    Draw rotated rectangles

--- 27-CoreGraphics/MyCoreGraphics/ViewController.swift
+++ 27-CoreGraphics/MyCoreGraphics/ViewController.swift
@@ -32,6 +32,8 @@ class ViewController: UIViewController {
       drawCircle()
     case 2:
       drawCheckerboard()
+    case 3:
+      drawRotatedSquares()
     default:
       break
     }
@@ -85,6 +87,27 @@ class ViewController: UIViewController {
           }
         }
       }
+    }
+
+    imageView.image = image
+  }
+
+  func drawRotatedSquares() {
+    let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+
+    let image = renderer.image { ctx in
+      ctx.cgContext.translateBy(x: 256, y: 256)
+
+      let rotations = 16
+      let amount = Double.pi / Double(rotations)
+
+      for _ in 0 ..< rotations {
+        ctx.cgContext.rotate(by: CGFloat(amount))
+        ctx.cgContext.addRect(CGRect(x: -128, y: -128, width: 256, height: 256))
+      }
+
+      ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+      ctx.cgContext.strokePath()
     }
 
     imageView.image = image
