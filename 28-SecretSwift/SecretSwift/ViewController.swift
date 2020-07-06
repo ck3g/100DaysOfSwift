0ad933755ec48e99ab050661d50a2fbaf8d8bb28
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-07-06 15:25:48 +0200

    Build a UI

--- 28-SecretSwift/SecretSwift/ViewController.swift
+++ 28-SecretSwift/SecretSwift/ViewController.swift
@@ -9,12 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  @IBOutlet var secret: UITextView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
 
 
+  @IBAction func authenticateTapped(_ sender: Any) {
+  }
 }
 
