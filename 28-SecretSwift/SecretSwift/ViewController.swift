2dc6979e6420a16a13d8967992ec743a9debbff1
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-07-07 09:48:57 +0200

    Authorize using biometry

--- 28-SecretSwift/SecretSwift/ViewController.swift
+++ 28-SecretSwift/SecretSwift/ViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 Vitali Tatarintev. All rights reserved.
 //
 
+import LocalAuthentication
 import UIKit
 
 class ViewController: UIViewController {
@@ -24,7 +25,29 @@ class ViewController: UIViewController {
 
 
   @IBAction func authenticateTapped(_ sender: Any) {
-    unlockSecretMessage()
+    // unlockSecretMessage()
+    let context = LAContext()
+    var error: NSError?
+
+    if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+      let reason = "Identify yourself!"
+
+      context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { [weak self] success, authenticationError in
+        DispatchQueue.main.async {
+          if success {
+            self?.unlockSecretMessage()
+          } else {
+            let ac = UIAlertController(title: "Authentication failed", message: "You could not be verified. Please, try again.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            self?.present(ac, animated: true)
+          }
+        }
+      }
+    } else {
+      let ac = UIAlertController(title: "Biometry unavailable", message: "Your device is not configured for biometric authentication.", preferredStyle: .alert)
+      ac.addAction(UIAlertAction(title: "OK", style: .default))
+      present(ac, animated: true)
+    }
   }
 
   @objc func adjustForKeyboard(notification: Notification) {
