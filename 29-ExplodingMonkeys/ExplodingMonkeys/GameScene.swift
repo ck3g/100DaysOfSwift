2ff75b502060e191bd492d30282ec098d899261b
Vitali Tatarintev <Kalastiuz@gmail.com>
2020-07-08 08:33:22 +0200

    Implement BuildingNode to draw a building

--- 29-ExplodingMonkeys/ExplodingMonkeys/GameScene.swift
+++ 29-ExplodingMonkeys/ExplodingMonkeys/GameScene.swift
@@ -8,6 +8,12 @@
 
 import SpriteKit
 
+enum CollisionTypes: UInt32 {
+  case banana = 1
+  case building = 2
+  case player = 4
+}
+
 class GameScene: SKScene {
 
   override func didMove(to view: SKView) {
